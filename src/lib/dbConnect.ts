40e10f3305ae1edbfdd3a40331caf2c8af879d04
Qasim <mianqasim065@gmail.com>
2025-12-06 22:45:27 +0500

    feat(db): setup robust mongodb connection utility

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,23 @@
+import mongoose, { Mongoose } from "mongoose";
+
+type connectionObject = {
+  isConnected?: number;
+};
+const connection: connectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database!");
+    return;
+  }
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI || "", {});
+    connection.isConnected = db.connections[0].readyState;
+    console.log("DB connected successfully!");
+  } catch (error) {
+    console.log("Database connection failed", console.error);
+
+    process.exit();
+  }
+}
+export default dbConnect;
